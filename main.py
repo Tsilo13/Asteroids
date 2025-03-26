e1b04ad824506983f1c44006aacd4e464610a7bd
Tsilo13 <gstoik59@gmail.com>
2025-03-26 13:06:10 -0400

    added player movement

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # display mode
     clock = pygame.time.Clock()
     running = True
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while running: # game loop
@@ -14,9 +15,12 @@ def main():
             if event.type == pygame.QUIT: # X button on window
                 running = False # exits infinite loop
 
+        player.update(dt)
+
         screen.fill("black") # screen color and fill
         player.draw(screen)
-        pygame.display.flip() # updates display
+        pygame.display.flip() # updates display]
+        
         dt = clock.tick(60) / 1000 # sets/limits the framerate to 60      
 
 
