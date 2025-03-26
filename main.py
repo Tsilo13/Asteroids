81496cf0f58d1ce83d55d5ea1ca4d76b68004da8
Tsilo13 <gstoik59@gmail.com>
2025-03-26 13:18:02 -0400

    added groups for objects and respective containers

--- main.py
+++ main.py
@@ -7,20 +7,29 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # display mode
     clock = pygame.time.Clock()
     running = True
+    updatable = pygame.sprite.Group() # group for objects that can be updated
+    drawable = pygame.sprite.Group() # group for objects that can be drawn
+    # add player containers to groups
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    # object groups 
+    
 
     while running: # game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # X button on window
                 running = False # exits infinite loop
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black") # screen color and fill
-        player.draw(screen)
+
+        for obj in drawable: # iterate over and draw each object in group
+            obj.draw(screen)
+
         pygame.display.flip() # updates display]
-        
+
         dt = clock.tick(60) / 1000 # sets/limits the framerate to 60      
 
 
